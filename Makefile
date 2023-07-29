a8137de795b2ce3a7dde81facd3073db75e9284c
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-07-29 00:32:40 -0300

    Feat: Create ft_toupper, this function receive a char and changes it to uppercase if it is lowercase.  Changes to be committed: 	modified:   Makefile 	new file:   ft_toupper.c 	modified:   libft.h

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/28 17:49:31 by dande-je          #+#    #+#              #
-#    Updated: 2023/07/28 22:21:29 by dande-je         ###   ########.org.br    #
+#    Updated: 2023/07/28 23:34:02 by dande-je         ###   ########.org.br    #
 #                                                                              #
 # **************************************************************************** #
 
 NAME := libft.a
 
-SRCS := ft_isalpha.c ft_isdigit.c ft_strlen.c ft_isalnum.c ft_isascii.c
+SRCS := ft_isalpha.c ft_isdigit.c ft_strlen.c ft_isalnum.c ft_isascii.c ft_toupper.c
 
 OBJS := $(SRCS:.c=.o)
 
@@ -27,16 +27,16 @@ RM := rm -rf
 all:	$(NAME)
 
 %.o:%.c
-	@$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -c $< -o $@
 
 $(NAME):	$(OBJS)
-	@$(AR) $(NAME) $(OBJS)
+	$(AR) $(NAME) $(OBJS)
 
 clean:
-	@$(RM) $(OBJS)
+	$(RM) $(OBJS)
 
 fclean:	clean
-	@$(RM) $(NAME)
+	$(RM) $(NAME)
 
 re:	fclean all
 
--- ft_toupper.c
+++ ft_toupper.c
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_toupper.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/24 19:26:57 by dande-je          #+#    #+#             */
-/*   Updated: 2023/07/29 00:02:16 by dande-je         ###   ########.org.br   */
+/*   Created: 2023/07/28 23:13:03 by dande-je          #+#    #+#             */
+/*   Updated: 2023/07/29 00:00:39 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-int		ft_toupper(int c);
-size_t	ft_strlen(const char *str);
-
-#endif
+int	ft_toupper(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (c - ('a' - 'A'));
+	return (c);
+}
