f5f6bb6329697abdabb7a948d13edd87e0974c5d
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-07-28 23:07:41 -0300

    Feat: Create ft_isascii, this function receive an int and verity if it is in the range of ascii table.  Changes to be committed: 	modified:   Makefile 	new file:   ft_isascii.c 	modified:   libft.h

--- Makefile
+++ Makefile
@@ -6,13 +6,13 @@
 #    By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/07/28 17:49:31 by dande-je          #+#    #+#              #
-#    Updated: 2023/07/28 19:26:48 by dande-je         ###   ########.org.br    #
+#    Updated: 2023/07/28 22:21:29 by dande-je         ###   ########.org.br    #
 #                                                                              #
 # **************************************************************************** #
 
 NAME := libft.a
 
-SRCS := ft_isalpha.c ft_isdigit.c ft_strlen.c ft_isalnum.c
+SRCS := ft_isalpha.c ft_isdigit.c ft_strlen.c ft_isalnum.c ft_isascii.c
 
 OBJS := $(SRCS:.c=.o)
 
--- ft_isascii.c
+++ ft_isascii.c
@@ -1,25 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/24 19:26:57 by dande-je          #+#    #+#             */
-/*   Updated: 2023/07/28 22:21:16 by dande-je         ###   ########.org.br   */
+/*   Created: 2023/07/28 22:06:31 by dande-je          #+#    #+#             */
+/*   Updated: 2023/07/28 22:20:53 by dande-je         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
+#include "libft.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-int		ft_isalnum(int c);
-int		ft_isascii(int c);
-size_t	ft_strlen(const char *str);
-
-#endif
+int	ft_isascii(int c)
+{
+	if (c >= 0 && c <= 127)
+		return (1);
+	return (0);
+}
