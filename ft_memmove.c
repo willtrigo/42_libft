a6056b3abc1394c862a81f50d9cdfce85d039946
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-17 04:58:26 -0300

    Feat: Create ft_memmove, this function is similiar of ft_memcpy, but this swap information is from end to begin of n.  Changes to be committed: 	new file:   ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/16 20:22:16 by dande-je          #+#    #+#             */
+/*   Updated: 2023/08/17 04:20:38 by dande-je         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	if (!dest && !src)
+		return (NULL);
+	if (dest < src)
+		return (ft_memcpy(dest, src, n));
+	while (n--)
+		*(char *)(dest + n) = *(char *)(src + n);
+	return (dest);
+}
