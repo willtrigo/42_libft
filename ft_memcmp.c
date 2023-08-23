d08175930383bfc17cb214d62a25e29681a8623d
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-23 03:29:27 -0300

    Fix: ft_memcmp, refactoring the code to get more performance.  Changes to be committed: 	modified:   ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 23:01:53 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/17 23:29:33 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/23 01:25:41 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,13 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t	i;
+	long int	i;
 
-	i = 0;
+	i = -1;
 	if (n == 0)
 		return (0);
-	while (n--)
-	{
+	while (++i < (long int)n)
 		if (*(unsigned char *)(s1 + i) != *(unsigned char *)(s2 + i))
 			return (*(unsigned char *)(s1 + i) - *(unsigned char *)(s2 + i));
-		i++;
-	}
 	return (0);
 }
