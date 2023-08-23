c27a5659ee837895f0d517d2612a9b5fbe7bb9bf
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-23 03:49:05 -0300

    Fix: ft_strmapi, refactoring the code to get more performance.  Changes to be committed: 	modified:   ft_strmapi.c

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 20:08:33 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/23 00:39:55 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/23 01:03:22 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
 	int		i;
-	int		s_len;
 	char	*str;
 
+	if (!s || !f)
+		return (NULL);
 	i = -1;
-	s_len = ft_strlen(s);
-	str = ft_calloc(s_len + 1, sizeof(char));
+	str = ft_calloc(ft_strlen(s) + 1, sizeof(char));
 	if (!str)
 		return (NULL);
-	while (++i < s_len)
+	while (s[++i])
 		str[i] = f(i, s[i]);
 	str[i] = '\0';
 	return (str);
