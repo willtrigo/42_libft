35feb3b46fab12ae8d5986dc1bef77d2bb506581
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-23 03:55:59 -0300

    Fix: ft_strnstr, refactoring the code to get mroe performance.  Changes to be committed: 	modified:   ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/13 23:12:41 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/15 21:06:23 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/23 01:43:17 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,9 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 		return ((char *)big);
 	while (*big && i <= len)
 	{
-		if (!ft_strncmp(big, little, little_len) && len - i >= little_len)
+		if (len - i++ >= little_len && !ft_strncmp(big, little, little_len))
 			return ((char *)big);
 		big++;
-		i++;
 	}
 	return (0);
 }
