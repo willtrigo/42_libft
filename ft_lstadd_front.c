1f1f0bd226f122bad0c36478e93e464ac33bb0d4
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-24 07:12:27 -0300

    Fix: ft_lstadd_front, fix the type of the function and remoce the pointer of the function.  Changes to be committed: 	modified:   ft_lstadd_front.c

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -6,13 +6,13 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/24 06:37:50 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/24 06:42:57 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/24 06:57:31 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-t_list	*ft_lstadd_front(t_list **lst, t_list *new)
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
 	if (!new)
 		return ;
