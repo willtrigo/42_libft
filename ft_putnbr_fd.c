fff1790cfe280e24d891bc574772accabc9fa1ed
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-24 06:32:04 -0300

    Fix: ft_putnbr_fd, fix the logic of print the int.  Changes to be committed: 	modified:   ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 04:53:30 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/23 05:12:31 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/24 06:29:31 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ void	ft_putnbr_fd(int n, int fd)
 		n_new *= -1;
 		ft_putchar_fd('-', fd);
 	}
-	if (n_new / 10 > 0)
+	if (n_new > 9)
 		ft_putnbr_fd(n_new / 10, fd);
-	ft_putchar_fd(n_new % (10 + '0'), fd);
+	ft_putchar_fd(n_new % 10 + '0', fd);
 }
