d8f95a83898159b88dd5348920cccae3fa8b3fbb
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-26 06:21:42 -0300

    Feat: ft_lstdelone, in this function receive two params, the first is the node of the list and the second is the function that is used to delete the node.  Changes to be committed: 	modified:   Makefile 	new file:   ft_lstdelone_bonus.c 	modified:   libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 19:26:57 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/25 00:39:55 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/26 06:17:10 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,5 +61,6 @@ void	ft_lstadd_front(t_list **lst, t_list *new);
 int		ft_lstsize(t_list *lst);
 t_list	*ft_lstlast(t_list *lst);
 void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 
 #endif
