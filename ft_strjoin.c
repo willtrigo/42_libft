dd9f59ead5d26d1db8e7d396bf3ec4a443d936ed
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2023-08-28 08:37:04 -0300

    Fix: ft_strjoin, change the logic of the verification.  Changes to be committed: 	modified:   ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 19:28:16 by dande-je          #+#    #+#             */
-/*   Updated: 2023/08/23 01:08:59 by dande-je         ###   ########.fr       */
+/*   Updated: 2023/08/28 07:29:45 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	size_t	s1_len;
 	size_t	s2_len;
 
-	if (!s1 || !s2)
-		return (NULL);
 	s1_len = ft_strlen(s1);
 	s2_len = ft_strlen(s2);
 	str = malloc((s1_len + s2_len + 1) * sizeof(char));
-	if (!str)
-		return (str);
+	if (str == NULL)
+		return (NULL);
 	ft_strlcpy(str, s1, s1_len + 1);
 	ft_strlcpy(&str[s1_len], s2, s2_len + 1);
 	return (str);
