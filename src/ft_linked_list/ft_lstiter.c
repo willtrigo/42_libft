d5c82b3d1e1c7718e9e61a15fd5f8552a2eb8c81
Daniel de Jesus Trigo <dande-je@student.42sp.org.br>
2024-03-10 04:52:31 -0300

    Fix: Appling the norminette in lstiter.c  Changes to be committed: 	modified:   src/ft_linked_list/ft_lstiter.c

--- src/ft_linked_list/ft_lstiter.c
+++ src/ft_linked_list/ft_lstiter.c
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 03:15:57 by dande-je          #+#    #+#             */
-/*   Updated: 2024/03/07 04:33:19 by dande-je         ###   ########.fr       */
+/*   Updated: 2024/03/10 04:51:51 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 /**
  * @brief Iterates the elements of the list.
  *
- * This function iterates the list `lst` and applies the function `f` on the content
- * of each node.
+ * This function iterates the list `lst` and applies the function `f` on the
+ * content of each node.
  *
  * @param lst The address of a pointer to a node.
  * @param f The address of the function used to iterate on the list.
